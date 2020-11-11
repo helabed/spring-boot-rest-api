a666ff58574f94853c3fc993a2b159a552a0f11b
Hani Elabed <hani.elabed@gmail.com>
2020-11-11 09:57:34 -0600

    adds comments

--- src/main/java/com/rubywebworks/springboot/quickstart/CourseApiApp.java
+++ src/main/java/com/rubywebworks/springboot/quickstart/CourseApiApp.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication   //This is a spring boot app
+@RestController          //This is a REST controller
 public class CourseApiApp {
 
 	public static void main(String[] args) {
+		// minimal spring boot app needs this line to declare this class as the entry point
 		SpringApplication.run(CourseApiApp.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("/hello")   //mapping /hello web url to this method, defaults to GET method.
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
