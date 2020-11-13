a26898b286a413aa4334cc4d1b6c39d2dbae8835
Hani Elabed <hani.elabed@gmail.com>
2020-11-12 22:20:20 -0600

    adds support for POSTing a topic.

--- src/main/java/com/rubywebworks/springboot/quickstart/topic/TopicController.java
+++ src/main/java/com/rubywebworks/springboot/quickstart/topic/TopicController.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-  
+
   @Autowired   // marks this instance as needing to be dependency injected into this class.
   private TopicService topicService;
 
@@ -24,5 +26,14 @@ public class TopicController {
   @RequestMapping("/topics/{topicId}")
   public Topic getTopic(@PathVariable("topicId") String id) {
     return topicService.getTopic(id);
+  }
+
+  // Example of a curl command to produce a POST
+  // curl -H "Accept: text/json" -H "Content-type: application/json" -d \
+  //   "{\"id\":\"ruby\",\"name\":\"Ruby on Rails\",\"description\":\"Introduction to Rails\"}" \
+  //   http://localhost:8080/topics
+  @RequestMapping(method=RequestMethod.POST, value="/topics")
+  public void addTopic(@RequestBody Topic topic) {
+    topicService.addTopic(topic);
   }
 }
--- src/main/java/com/rubywebworks/springboot/quickstart/topic/TopicService.java
+++ src/main/java/com/rubywebworks/springboot/quickstart/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.rubywebworks.springboot.quickstart.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-  private List<Topic> topics = Arrays.asList(
+  private List<Topic> topics = new ArrayList<>(Arrays.asList(
       new Topic("spring", "Spring Framework", "Introduction to the Spring Framework"),
       new Topic("java", "Core Java", "Introduction to Core Java"),
       new Topic("javascript", "Javascript Programming", "Introduction to JavaScript Programming")
-    );
+    ));
 
   public List<Topic> getAllTopics() {
     return topics;
@@ -20,5 +21,9 @@ public class TopicService {
 
   public Topic getTopic(String id) {
     return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+  }
+
+  public void addTopic(Topic topic) {
+    topics.add(topic);
   }
 }
